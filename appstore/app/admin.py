ab89d3959be5d8dc18ee6e61cbb8346e9c4031af
’htcao‘ <hangting@umich.edu>
2019-11-17 22:42:25 -0500

    update app_detail

--- appstore/app/admin.py
+++ appstore/app/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from app.models import App, Developer
+from app.models import App, Developer, Reviewer, Language
 
 # Register your models here.
 admin.site.register(App)
 admin.site.register(Developer)
+admin.site.register(Reviewer)
+admin.site.register(Language)
