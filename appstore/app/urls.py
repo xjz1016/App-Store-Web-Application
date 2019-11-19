e7c1449df983d16a3658b044145b5abab29c7245
’htcao‘ <hangting@umich.edu>
2019-11-19 14:28:02 -0500

    revise the typo

--- appstore/app/urls.py
+++ appstore/app/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('', views.AppListView.as_view(), name='all'),
     path('app/create/', views.AppCreate.as_view(), name='app_create'),
     path('category/create/', views.CategoryCreate.as_view(), name='category_create'),
-    path('language/creat/', views.LanguageCreate.as_view(), name='language_create'),
+    path('language/create/', views.LanguageCreate.as_view(), name='language_create'),
     path('app_picture/<int:pk>', views.stream_file, name='app_picture'),
     path('app/<int:pk>', views.AppDetailView.as_view(), name='app_detail'),
 ]
